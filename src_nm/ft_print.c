36fa76ee63ee69b0a8cebfded6e163127ad46de9
Florent <Onepoint@Florents-iMac.local>
2016-04-08 11:55:33 +0200

    print

--- src_nm/ft_print.c
+++ src_nm/ft_print.c
@@ -1,5 +1,7 @@
 #include "nm.h"
 
+#include <stdio.h>
+
 void	ft_print_nm(int nsyms, int symoff, int stroff, char *ptr)
 {
 	int				i;
@@ -11,7 +13,20 @@ void	ft_print_nm(int nsyms, int symoff, int stroff, char *ptr)
 	i = 0;
 	while (i < nsyms)
 	{
-		ft_printf("%s\n", stringtable + symtable[i].n_un.n_strx);
+		unsigned int	maj = (symtable[i].n_value >> 32);
+		unsigned int	min = symtable[i].n_value;
+		int				sect = symtable[i].n_sect;
+		if (maj == 0 && min == 0)
+			printf("                 ");
+		else
+			printf("%08x%08x ", maj, min);
+		if (sect == 0)
+			printf("U ");
+		else if (sect == 1)
+			printf("T ");
+		else
+			printf("L");
+		printf("%s\n", stringtable + symtable[i].n_un.n_strx);
 		i++;
 	}
 }
